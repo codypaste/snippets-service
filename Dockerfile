cf51c479dcceae6746390ce06677bc4e3c4df512
Daniel Kawka <daniel.kawka@outlook.com>
2019-03-20 22:28:47 +0000

    * Remove redis * Remove mongodb leftovers * Update dependencies

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ WORKDIR /usr/src/app
 ADD ./service/package.json /usr/src/app/package.json
 
 RUN npm install --quiet --production --no-progress --registry=${registry:-https://registry.npmjs.org} && npm cache clean --force
+RUN npm install -g prisma@1.28.3
 
 ADD ./service /usr/src/app
 
--- service/src/database/generated/prisma-client/index.js
+++ service/src/database/generated/prisma-client/index.js
@@ -1,25 +1,25 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var prisma_lib_1 = require('prisma-client-lib');
-var typeDefs = require('./prisma-schema').typeDefs;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
-    name: 'Group',
-    embedded: false,
+    name: "Group",
+    embedded: false
   },
   {
-    name: 'Snippet',
-    embedded: false,
+    name: "Snippet",
+    embedded: false
   },
   {
-    name: 'SnippetChunk',
-    embedded: false,
-  },
+    name: "SnippetChunk",
+    embedded: false
+  }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env['PRISMA_ENDPOINT']}`,
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
